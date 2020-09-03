effcc1a3ff8a6d0554bb688f6c5b9b57d975379b
josephlee3454 <joseph.lee3454@gmail.com>
2020-09-02 19:54:59 -0700

    added email to login

--- users/users/models.py
+++ users/users/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-  pass
+  email = models.EmailField(('email address'), blank=True, unique=True)
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['username']
 
   def __str__(self):
     return self.username
